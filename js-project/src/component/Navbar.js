1c37cc14aaef64ff1e7f03e7da5a11a2ef75f553
minoolee <yaqoubiamina@gmail.com>
2022-11-16 20:03:18 +0100

    change

--- js-project/src/component/Navbar.js
+++ js-project/src/component/Navbar.js
@@ -3,22 +3,22 @@ import Logo from '../Ases/logo.png'
 function App() {
  
   return (
-    <div className="App">
-        <div className='w-full md:flex justify-between p-4 bg-red-200 text-white text-2xl'>
+  
+        <div className='w-full flex justify-between items-center p-5 bg-red-200 text-white text-2xl'>
             <div>
                 <img className='w-20' src={Logo} alt="" />
             </div>
-    <menu className='w-96' >
-      <ul className='flex justify-around items-center p-4'>
+      <menu className='w-96'>
+      <ul className='hidden md:flex justify-between'>
         <li>Home</li>
         <li>Service</li>
         <li>Contact</li>
         <li>Info</li>
       </ul>
-    </menu>
+      </menu>
    
     </div>
-    </div>
+  
   );
 }
 
